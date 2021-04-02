55724e992483ffcbead797095f05d2c55153209f
Ayan Sarkar <ayansarkar367@gmail.com>
2021-04-02 08:32:10 +0530

    add action creators for user signup

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -2,5 +2,6 @@ const API_ROOT = 'http://localhost:8080/api/v1';
 
 export const APIUrls = {
   login: () => `${API_ROOT}/users/create-session`,
+  signup: () => `${API_ROOT}/users/register-user`,
   fetchPosts: () => `${API_ROOT}/posts/`,
 };
