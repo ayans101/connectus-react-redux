c2c9b60cf03282ef70584da10b9ba5466943ae7a
Ayan Sarkar <ayansarkar367@gmail.com>
2021-04-09 23:59:41 +0530

    add UI for Chat component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import { PostsList, FriendsList } from './';
+import { PostsList, FriendsList, Chat } from './';
 
 class Home extends Component {
-    render() {
-        const { posts, friends, isLoggedin } = this.props;
-        console.log('PROPS', this.props);
-        return (
-            <div className="home">
-                <PostsList posts={posts} />
-                {isLoggedin && <FriendsList friends={friends} />}
-            </div>
-        );
-    }
+  render() {
+    const { posts, friends, isLoggedin } = this.props;
+    console.log('PROPS', this.props);
+    return (
+      <div className="home">
+        <PostsList posts={posts} />
+        {isLoggedin && <FriendsList friends={friends} />}
+        {isLoggedin && <Chat />}
+      </div>
+    );
+  }
 }
 
 export default Home;
